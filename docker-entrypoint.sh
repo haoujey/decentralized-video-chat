f120b0b98b4fb8591ae54e7c420157ac1f0cea7f
Amir Rahwane <amir@branchlabs.com>
2020-04-13 14:58:37 +0300

    use dockerhub image + remove testing networks from compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,19 @@ set -e
 ENVFILE=/opt/zipcall/.env
 
 if test -f "$ENVFILE"; then
-    echo "$ENVFILE exist"
     exec "$@"
 fi
 
 if [ -z "$TWILIO_ACCOUNT_SID" ]
 then
   touch $ENVFILE
-  echo "$TWILIO_ACCOUNT_SID" >> $ENVFILE
+  echo "TWILIO_ACCOUNT_SID=$TWILIO_ACCOUNT_SID" >> $ENVFILE
 fi
 
 if [ -z "$LOCAL_AUTH_TOKEN" ]
 then
   touch $ENVFILE
-  echo "$LOCAL_AUTH_TOKEN" >> $ENVFILE
+  echo "LOCAL_AUTH_TOKEN=$LOCAL_AUTH_TOKEN" >> $ENVFILE
 fi
 
 exec "$@"
